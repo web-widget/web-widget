1f498ef565ae717c3097fc48a3329324f42251da
糖饼 <sugarpie.tang@gmail.com>
2021-06-09 12:17:07 +0800

    chore: 使用 web-dev-server

---
+++ examples/web-widget-router/web-dev-server.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  open: true,
+  nodeResolve: true,
+  appIndex: 'index.html',
+  watch: true,
+  // in a monorepo you need to set set the root dir to resolve modules
+  rootDir: './',
+};
