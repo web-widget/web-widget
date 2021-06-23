a87a8af913e8f96944bf19c5df2310679b8fde2e
糖饼 <sugarpie.tang@gmail.com>
2021-06-23 11:23:28 +0800

    refactor: 简化逻辑

--- src/WebWidget/lifecycles/mount.js
+++ src/WebWidget/lifecycles/mount.js
@@ -1,6 +1,5 @@
 import { NOT_MOUNTED, MOUNTED, MOUNT_ERROR } from '../applications/status.js';
 import { reasonableTime } from '../applications/timeouts.js';
-import { toUnmountPromise } from './unmount.js';
 import { formatErrorMessage } from '../applications/errors.js';
 
 export async function toMountPromise(model) {
@@ -18,8 +17,6 @@ export async function toMountPromise(model) {
       model.unmountPromise = null;
     })
     .catch(async error => {
-      model.status = MOUNTED;
-      await toUnmountPromise(model);
       model.status = MOUNT_ERROR;
       model.mountPromise = null;
       throw formatErrorMessage(model, error);
