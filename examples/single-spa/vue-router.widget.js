36448f46f120f8537957668f34ef575bbf2a735e
糖饼 <sugarpie.tang@gmail.com>
2021-07-07 16:50:17 +0800

    docs

--- examples/single-spa/vue-router.widget.js
+++ examples/single-spa/vue-router.widget.js
@@ -1,11 +1,23 @@
 /* eslint-disable import/no-unresolved */
 
-let main, nav, Vue, VueRouter, app;
+let main, nav, Vue, VueRouter, app, appRoot;
 console.log('Vue load');
 export default {
   async bootstrap({ container }) {
     console.log('Vue router bootstrap');
 
+    nav = document.createElement('web-widget');
+    nav.src = '/nav.widget.js';
+    nav.inactive = true;
+    container.appendChild(nav);
+    main = document.createElement('main');
+    main.innerHTML = `
+        <h3>Vue router</h3>
+      `;
+
+    await nav.bootstrap();
+
+    // bootstrap vue app
     await import('https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js');
     await import(
       'https://cdn.jsdelivr.net/npm/vue-router@3.5.2/dist/vue-router.js'
@@ -14,10 +26,32 @@ export default {
     VueRouter = window.VueRouter;
     delete window.Vue;
     delete window.VueRouter;
+  },
+  async mount({ container }) {
+    console.log('Vue router mount');
+    await nav.mount();
+    container.appendChild(main);
 
-    main = document.createElement('main');
-    main.innerHTML = `
-        <h3>Vue router</h3>
+    // mount vue app
+    Vue.use(VueRouter);
+    const Foo = { template: '<div>foo</div>' };
+    const Bar = { template: '<div>bar</div>' };
+    const routes = [
+      { path: '/vue-router/foo', component: Foo },
+      { path: '/vue-router/bar', component: Bar }
+    ];
+
+    const router = new VueRouter({
+      mode: 'history',
+      routes
+    });
+
+    app = new Vue({
+      router
+    });
+
+    appRoot = document.createElement('div');
+    appRoot.innerHTML = `
         <div id="app">
           <h1>Hello App!</h1>
           <p>
@@ -27,45 +61,18 @@ export default {
           <router-view></router-view>
         </div>
       `;
-
-    nav = document.createElement('web-widget');
-    nav.src = '/nav.widget.js';
-    nav.inactive = true;
-    container.appendChild(nav);
-
-    return nav.bootstrap();
-  },
-  async mount({ container }) {
-    console.log('Vue router mount');
-    await nav.mount();
-    container.appendChild(main);
-
-    if (!app) {
-      const Foo = { template: '<div>foo</div>' };
-      const Bar = { template: '<div>bar</div>' };
-      const routes = [
-        { path: '/vue-router/foo', component: Foo },
-        { path: '/vue-router/bar', component: Bar }
-      ];
-
-      const router = new VueRouter({
-        mode: 'history',
-        routes
-      });
-
-      app = new Vue({
-        router
-      });
-    }
-
-    app.$mount(container.querySelector('#app'));
+    container.appendChild(appRoot);
+    app.$mount(appRoot);
   },
   async unmount({ container }) {
     console.log('Vue router unmount');
     await nav.unmount();
-
-    app.$destroy('#app');
-
     container.removeChild(main);
+
+    // unmount vue app
+    app.$destroy();
+    app.$el.parentNode.removeChild(app.$el);
+    app = null;
+    appRoot = null;
   }
 };
