00a45f0a422e256b7692d0f107c674d10ff4cd0b
糖饼 <sugarpie.tang@gmail.com>
2021-10-22 23:45:23 +0800

    chore

--- scripts/tester/src/index.js
+++ scripts/tester/src/index.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-undef */
-// const port = require('./getPort');
-
 module.exports = {
   files: ['test/**/*.test.js'],
   nodeResolve: true,
   rootDir: './',
-  // port,
   coverageConfig: {
     include: ['src/**/*']
   }
