b25efa8f283c4f7b838600c3fd0ac861f6f19e81
糖饼 <sugarpie.tang@gmail.com>
2021-11-14 19:33:13 +0800

    fix

--- packages/container/src/index.js
+++ packages/container/src/index.js
@@ -1,3 +1,3 @@
-export { HTMLWebWidgetElement, run } from './HTMLWebWidgetElement.js';
+export { HTMLWebWidgetElement, bootstrap } from './HTMLWebWidgetElement.js';
 export { WebWidgetDependencies } from './WebWidgetDependencies.js';
 export { WebWidgetSandbox } from './WebWidgetSandbox.js';
