7e5d38fd0a44ec78d4049a04777179c2ab760941
糖饼 <sugarpie.tang@gmail.com>
2021-12-05 16:54:33 +0800

    refactor: data

--- packages/container/test/WebWidgetDependencies.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from '@esm-bundle/chai';
-import { WebWidgetDependencies } from '../src/WebWidgetDependencies.js';
-
-describe('WebWidgetDependencies', () => {
-  it('It should be a function', () => {
-    expect(WebWidgetDependencies).to.be.a('function');
-  });
-});
