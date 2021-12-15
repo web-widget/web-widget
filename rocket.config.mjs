431b468cd1c895cd66b8a4b9e882c4523ef893db
糖饼 <sugarpie.tang@gmail.com>
2021-12-15 10:51:19 +0800

    chore: 升级文档构建工具

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -1,11 +1,10 @@
 import { rocketLaunch } from '@rocket/launch';
-import { rocketBlog } from '@rocket/blog';
 import { rocketSearch } from '@rocket/search';
 // eslint-disable-next-line import/no-unresolved
 import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
 
 export default {
-  presets: [rocketLaunch(), rocketBlog(), rocketSearch()],
+  presets: [rocketLaunch(), rocketSearch()],
   emptyOutputDir: false,
   absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8000')
 };
