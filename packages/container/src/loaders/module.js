76bab2e896bd5d84f36984000b3f6cbcecdd051c
糖饼 <sugarpie.tang@gmail.com>
2022-03-15 16:43:49 +0800

    refactor: 性能优化

--- packages/container/src/loaders/module.js
+++ packages/container/src/loaders/module.js
@@ -1,12 +1,16 @@
-/* global Blob, URL */
+/* global Blob, URL, HTMLScriptElement */
+
+// @see https://github.com/WICG/import-maps#feature-detection
+const supportsImportMaps =
+  HTMLScriptElement.supports && HTMLScriptElement.supports('importmap');
 
 function getModuleValue(module) {
   return module.default || module;
 }
 
 function importModule(target) {
-  // @see https://github.com/guybedford/es-module-shims
-  if (typeof importShim === 'function') {
+  if (!supportsImportMaps && typeof importShim === 'function') {
+    // @see https://github.com/guybedford/es-module-shims
     // eslint-disable-next-line no-undef
     return importShim(target);
   }
