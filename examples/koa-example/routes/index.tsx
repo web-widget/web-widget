d8349cb1dc3a0304caf2a3621d63d7258fb93f2a
waisiukei <wakiki@outlook.com>
2023-06-29 16:49:34 +0800

    chore: css module 的 demo

--- examples/koa-example/routes/index.tsx
+++ examples/koa-example/routes/index.tsx
@@ -1,14 +1,14 @@
 import ReactCounter from "../islands/Counter.tsx";
 import VueCounter from "../islands/CounterVue.ts";
 import { render } from "@web-widget/react";
-import "./index.css";
+import styles from "./index.module.css";
 
 export { render };
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className={styles.title}>Home</h1>
       <p>
         Welcome to widget web server. Try to update this message in the
         ./routes/index.tsx file, and refresh.
