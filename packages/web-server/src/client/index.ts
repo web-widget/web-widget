5c97437015ea2751df8c2fc2e4a69d1571eab867
糖饼 <sugarpie.tang@gmail.com>
2023-07-27 23:41:19 +0800

    chore: 整理文件

--- packages/web-server/src/client/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "@web-widget/web-widget";
-export type * from "@web-widget/web-widget";
-
-export default function router() {
-  throw new Error(`Client routing is not supported.`);
-}
