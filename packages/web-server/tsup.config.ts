468583009933b2460de6ce22aa8911384028f47f
糖饼 <sugarpie.tang@gmail.com>
2023-07-27 23:41:48 +0800

    chore: 整理文件 (#84)

--- packages/web-server/tsup.config.ts
+++ packages/web-server/tsup.config.ts
@@ -1,8 +1,7 @@
 import type { Options } from "tsup";
 export const tsup: Options = {
   entry: {
-    server: "src/server/index.ts",
-    client: "src/client/index.ts",
+    server: "src/index.ts",
   },
   dts: true,
   target: "es2022",
