72e991b1518f04e97daee9f774c967d91f420a0c
糖饼 <sugarpie.tang@gmail.com>
2023-07-30 21:20:19 +0800

    chore: 增加 deno 的示例 (#101)

--- packages/web-router/src/index.ts
+++ packages/web-router/src/index.ts
@@ -1,19 +1,22 @@
 import { ServerContext } from "./context";
-import type { StartOptions, ServerHandler, ServerConnInfo } from "./types";
+import type {
+  StartOptions,
+  ServerHandler,
+  ServerConnInfo,
+  Manifest,
+} from "./types";
 export type * from "./types";
 
 export default class WebRouter {
   #handler: ServerHandler;
 
-  constructor(manifestUrl: string | URL, opts: StartOptions = {}) {
-    const promise = ServerContext.fromManifest(
-      manifestUrl,
-      opts,
-      !!opts.dev
-    ).then((serverContext) => {
-      this.#handler = serverContext.handler();
-      return serverContext;
-    });
+  constructor(routemap: string | URL | Manifest, opts: StartOptions = {}) {
+    const promise = ServerContext.fromManifest(routemap, opts, !!opts.dev).then(
+      (serverContext) => {
+        this.#handler = serverContext.handler();
+        return serverContext;
+      }
+    );
 
     this.#handler = async (req: Request, connInfo: ServerConnInfo = {}) => {
       const serverContext = await promise;
--- packages/web-router/tsup.config.ts
+++ packages/web-router/tsup.config.ts
@@ -1,7 +1,7 @@
 import type { Options } from "tsup";
 export const tsup: Options = {
   entry: {
-    server: "src/index.ts",
+    "web-router.server": "src/index.ts",
   },
   dts: true,
   target: "es2022",
