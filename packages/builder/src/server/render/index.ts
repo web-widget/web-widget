203b34e3c4f16a098d3edae6a68db75a80df738f
糖饼 <sugarpie.tang@gmail.com>
2023-08-12 18:53:34 +0800

    refactor: 简化 dev 模式获取 meta (#142)

--- packages/builder/src/server/render/index.ts
+++ packages/builder/src/server/render/index.ts
@@ -1,20 +1,24 @@
 import type { ModuleLoader } from "../loader/index";
+import type {
+  LinkDescriptor,
+  StyleDescriptor,
+  ScriptDescriptor,
+} from "@web-widget/schema";
 import { getStylesForURL } from "./css";
 import { viteID } from "./util";
 
-export interface SSRElement {
-  props: Record<string, any>;
-  children: string;
-}
-
-export async function getAssets(
+export async function getMeta(
   filePath: URL,
   loader: ModuleLoader,
   root: URL,
   mode: string
-) {
+): Promise<{
+  link: LinkDescriptor[];
+  style: StyleDescriptor[];
+  script: ScriptDescriptor[];
+}> {
   // Add hoisted script tags
-  const scripts = new Set<SSRElement>();
+  const script: ScriptDescriptor[] = [];
 
   // Pass framework CSS in as style tags to be appended to the page.
   const { urls: styleUrls, stylesMap } = await getStylesForURL(
@@ -22,37 +26,25 @@ export async function getAssets(
     loader,
     mode
   );
-  let links = new Set<SSRElement>();
-  [...styleUrls].forEach((href) => {
-    links.add({
-      props: {
-        rel: "stylesheet",
-        href,
-      },
-      children: "",
-    });
-  });
+  let link: LinkDescriptor[] = [...styleUrls].map((href) => ({
+    rel: "stylesheet",
+    href,
+  }));
 
-  let styles = new Set<SSRElement>();
-  [...stylesMap].forEach(([url, content]) => {
+  let style: StyleDescriptor[] = [...stylesMap].map(([url, content]) => {
     // Vite handles HMR for styles injected as scripts
-    scripts.add({
-      props: {
-        type: "module",
-        src: url,
-      },
-      children: "",
+    script.push({
+      type: "module",
+      src: url,
     });
     // But we still want to inject the styles to avoid FOUC
-    styles.add({
-      props: {
-        type: "text/css",
-        // Track the ID so we can match it to Vite's injected style later
-        "data-vite-dev-id": viteID(new URL(`.${url}`, root)),
-      },
-      children: content,
-    });
+    return {
+      // Track the ID so we can match it to Vite's injected style later
+      // @ts-ignore
+      "data-vite-dev-id": viteID(new URL(`.${url}`, root)),
+      content,
+    };
   });
 
-  return { scripts, styles, links };
+  return { script, style, link };
 }
