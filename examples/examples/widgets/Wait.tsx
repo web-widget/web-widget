2d5088a171d0bbaf911195d2afe5cbc33a6ef9d4
糖饼 <sugarpie.tang@gmail.com>
2023-08-13 14:56:37 +0800

    streaming: 重构流式传输的示例

--- examples/examples/widgets/Wait.tsx
+++ examples/examples/widgets/Wait.tsx
@@ -2,11 +2,16 @@ export { render } from "@web-widget/react";
 
 export type WaitProps = { timeout: number };
 
+const fetchData = async (timeout: number) =>
+  await new Promise((resolve) =>
+    setTimeout(() => resolve(`Wait: ${timeout}ms`), timeout)
+  );
+
 export default (async function Wait({ timeout }: WaitProps) {
-  await new Promise((resolve) => setTimeout(resolve, timeout));
+  const data = (await fetchData(timeout)) as string;
   return (
     <>
-      <p>Wait: {timeout}ms</p>
+      <p>{data}</p>
     </>
   );
 } as unknown as React.FunctionComponent<any>);
