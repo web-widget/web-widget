625a9fe7df0970442f268be67fe940c56a52a64c
糖饼 <sugarpie.tang@gmail.com>
2023-08-13 14:57:13 +0800

    streaming: 重构流式传输的示例 (#144)

--- examples/examples/routes/streaming.tsx
+++ examples/examples/routes/streaming.tsx
@@ -6,12 +6,17 @@ export { render } from "@web-widget/react";
 const Loading = <div>Loading..</div>;
 
 export default async function Page() {
+  const tips = import.meta.env.DEV ? (
+    <p style={{ background: "yellow" }}>
+      💡 This instance needs to run `pnpm start`
+    </p>
+  ) : (
+    <></>
+  );
   return (
     <BaseLayout>
       <h1>Streaming</h1>
-      <p style={{ background: "yellow" }}>
-        💡 This instance needs to run `pnpm start`
-      </p>
+      {tips}
       <Wait as="web-widget" fallback={Loading} timeout={1000} />
       <Wait as="web-widget" fallback={Loading} timeout={2000} />
       <Wait as="web-widget" fallback={Loading} timeout={3000} />
