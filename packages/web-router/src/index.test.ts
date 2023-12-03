16ac00e7ba655c7359120f2b7aebb900a8219c30
糖饼 <sugarpie.tang@gmail.com>
2023-12-03 20:04:26 +0800

    fix: Hitting multiple routes should only render the first one

--- packages/web-router/src/index.test.ts
+++ packages/web-router/src/index.test.ts
@@ -1,4 +1,6 @@
+import type { LayoutModule } from ".";
 import WebRouter from ".";
+import * as defaultLayoutModule from "./layout";
 
 describe("Basic", () => {
   const app = new WebRouter({
@@ -7,7 +9,7 @@ describe("Basic", () => {
         pathname: "/hello",
         module: {
           handler: {
-            GET(context) {
+            GET() {
               return new Response("get hello");
             },
             POST() {
@@ -18,7 +20,7 @@ describe("Basic", () => {
       },
     ],
     layout: {
-      module: {},
+      module: defaultLayoutModule as LayoutModule,
     },
     middlewares: [],
     fallbacks: [],
@@ -38,5 +40,56 @@ describe("Basic", () => {
     expect(res).not.toBeNull();
     expect(res.status).toBe(200);
     expect(await res.text()).toBe("post hello");
+  });
+});
+
+describe("Multiple identical routes", () => {
+  const app = new WebRouter({
+    routes: [
+      {
+        pathname: "/:lang?/",
+        module: {
+          handler: () => new Response("Home"),
+        },
+      },
+      {
+        pathname: "/:lang?/a/",
+        module: {
+          handler: () => new Response("a"),
+        },
+      },
+      {
+        pathname: "/:lang?/b/",
+        module: {
+          handler: () => new Response("b"),
+        },
+      },
+    ],
+    layout: {
+      module: defaultLayoutModule as LayoutModule,
+    },
+    middlewares: [],
+    fallbacks: [],
+  });
+
+  it("GET http://localhost/ is ok", async () => {
+    const res = await app.request("http://localhost/");
+    expect(res).not.toBeNull();
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("Home");
+  });
+
+  it("POST http://localhost/a/ is ok", async () => {
+    const res = await app.request("http://localhost/a/");
+    expect(res).not.toBeNull();
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("Home");
+  });
+
+  it("POST http://localhost/b/ is ok", async () => {
+    const res = await app.request("http://localhost/b/");
+    expect(res).not.toBeNull();
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("Home");
   });
 });
