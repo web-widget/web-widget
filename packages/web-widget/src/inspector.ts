fc782c316a7cc1af1b344606d1e7cfd99b6bbba9
糖饼 <sugarpie.tang@gmail.com>
2023-12-19 17:34:58 +0800

    refactor: web-widget-inspector

--- packages/web-widget/src/inspector.ts
+++ packages/web-widget/src/inspector.ts
@@ -22,55 +22,6 @@ function openInEditor(path: string, srcDir: string) {
   }
 }
 
-function updateOverlay(
-  hoverElement: HTMLElement | undefined,
-  overlay: HTMLElement,
-  isActive: () => boolean
-) {
-  if (hoverElement && isActive()) {
-    const rect = findBox(hoverElement).getBoundingClientRect();
-    overlay.style.setProperty("height", rect.height + "px");
-    overlay.style.setProperty("width", rect.width + "px");
-    overlay.style.setProperty("top", rect.top + "px");
-    overlay.style.setProperty("left", rect.left + "px");
-    overlay.style.setProperty("visibility", "visible");
-    document.body.style.setProperty("cursor", "pointer");
-    hoverElement.title = getDebugInfo(hoverElement);
-  } else {
-    overlay.style.setProperty("height", "0px");
-    overlay.style.setProperty("width", "0px");
-    overlay.style.setProperty("visibility", "hidden");
-    document.body.style.removeProperty("cursor");
-    hoverElement && (hoverElement.title = "");
-  }
-}
-
-function getDebugInfo(element: HTMLElement & { performance?: any }) {
-  let log = "web-widget[name=" + element.getAttribute("name") + "]";
-
-  if (element.performance) {
-    const labels = Object.keys(element.performance).map((key) =>
-      key.replace(/([A-Z])/g, " $1").toLowerCase()
-    );
-    const titleMaxLength = Math.max(...labels.map((key) => key.length));
-    const debugInfo = Object.values(element.performance).reduce(
-      (log, value, index) => {
-        log =
-          log +
-          "\n" +
-          labels[index].padEnd(titleMaxLength + 4, "\u2002") +
-          ": " +
-          value;
-        return log;
-      },
-      ""
-    );
-    log += "\n" + debugInfo;
-  }
-
-  return log;
-}
-
 export class HTMLWebWidgetInspectorElement extends HTMLElement {
   get dir() {
     return this.getAttribute("dir") || "";
@@ -88,136 +39,151 @@ export class HTMLWebWidgetInspectorElement extends HTMLElement {
     this.setAttribute("keys", JSON.stringify(value));
   }
 
+  #pressedKeys: Set<string> = new Set();
+  #hoveredElement?: HTMLElement;
+
+  #addKeysHandleEvent(event: KeyboardEvent) {
+    this.#pressedKeys.add(event.code);
+    this.#updateOverlay();
+  }
+
+  #deleteKeysHandleEvent(event: KeyboardEvent) {
+    this.#pressedKeys.delete(event.code);
+    this.#updateOverlay();
+  }
+
+  #clearKeysHandleEvent() {
+    this.#pressedKeys.clear();
+    this.#updateOverlay();
+  }
+
+  #hoveredHandleEvent(event: MouseEvent) {
+    const target = findContainer(event.target as HTMLElement);
+    if (target) {
+      this.#hoveredElement = target;
+    } else {
+      this.#hoveredElement = undefined;
+    }
+    this.#updateOverlay();
+  }
+
+  #clickHandEvent(event: MouseEvent) {
+    if (this.#isActive()) {
+      this.#pressedKeys.clear();
+      const target = findContainer(event.target as HTMLElement);
+      const inspectUrl = target?.getAttribute("import");
+      if (inspectUrl) {
+        event.preventDefault();
+        document.body.style.setProperty("cursor", "progress");
+        openInEditor(inspectUrl, this.dir);
+      }
+    }
+  }
+
+  #updateOverlayHandEvent() {
+    this.#updateOverlay();
+  }
+
+  #isActive() {
+    const activeKeys = Array.from(this.#pressedKeys).map((key) =>
+      key ? key.replace(/(Left|Right)$/g, "") : undefined
+    );
+    return this.keys.every((key) => activeKeys.includes(key));
+  }
+
+  #updateOverlay() {
+    let overlay = this.querySelector(
+      "web-widget-inspector-overlay"
+    ) as HTMLElement;
+
+    if (!overlay) {
+      overlay = document.createElement("web-widget-inspector-overlay");
+      overlay.setAttribute("aria-hidden", "true");
+      this.appendChild(overlay);
+    }
+
+    if (this.#hoveredElement && this.#isActive()) {
+      const rect = findBox(this.#hoveredElement).getBoundingClientRect();
+      overlay.style.setProperty("height", rect.height + "px");
+      overlay.style.setProperty("width", rect.width + "px");
+      overlay.style.setProperty("top", rect.top + "px");
+      overlay.style.setProperty("left", rect.left + "px");
+      overlay.style.setProperty("visibility", "visible");
+      document.body.style.setProperty("cursor", "pointer");
+    } else {
+      overlay.style.setProperty("height", "0px");
+      overlay.style.setProperty("width", "0px");
+      overlay.style.setProperty("visibility", "hidden");
+      document.body.style.removeProperty("cursor");
+    }
+  }
+
+  #showInfo() {
+    console.debug(
+      "%c ⇱ Web Widget Click-To-Source ",
+      "background: linear-gradient(315deg,#afd760 25%,#0074a6); color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;",
+      "Hold-press the " +
+        this.keys.join(" + ") +
+        " key" +
+        ((this.keys.length > 1 && "s") || "") +
+        " and click a component to jump directly to the source code in your IDE!"
+    );
+  }
+
   connectedCallback() {
     const hotKeys = this.keys;
-    const srcDir = this.dir;
 
     if (!document.querySelectorAll("web-widget[import]").length) {
       return;
     }
 
-    this.appendChild(
-      Object.assign(document.createElement("web-widget-inspector-info-popup"), {
-        textContent: "Click-to-Source: " + hotKeys.join(" + "),
-      })
-    );
-
-    console.debug(
-      "%c ⇱ Web Widget Click-To-Source ",
-      "background: linear-gradient(315deg,#afd760 25%,#0074a6); color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;",
-      "Hold-press the " +
-        hotKeys.join(" + ") +
-        " key" +
-        ((hotKeys.length > 1 && "s") || "") +
-        " and click a component to jump directly to the source code in your IDE!"
-    );
-
-    const inspectorState: {
-      pressedKeys: Set<string>;
-      hoveredElement?: HTMLElement;
-    } = {
-      pressedKeys: new Set(),
-    };
-
-    const overlay = document.createElement("web-widget-inspector-overlay");
-    overlay.setAttribute("aria-hidden", "true");
-    this.appendChild(overlay);
-
-    document.addEventListener(
-      "keydown",
-      (event) => {
-        inspectorState.pressedKeys.add(event.code);
-        updateOverlay(inspectorState.hoveredElement, overlay, isActive);
-      },
-      { capture: true }
-    );
-
-    document.addEventListener(
-      "keyup",
-      (event) => {
-        inspectorState.pressedKeys.delete(event.code);
-        updateOverlay(inspectorState.hoveredElement, overlay, isActive);
-      },
-      { capture: true }
-    );
-
-    window.addEventListener(
-      "blur",
-      () => {
-        inspectorState.pressedKeys.clear();
-        updateOverlay(inspectorState.hoveredElement, overlay, isActive);
-      },
-      { capture: true }
-    );
-
-    document.addEventListener(
-      "mouseover",
-      (event) => {
-        const target = findContainer(event.target as HTMLElement);
-        if (target) {
-          inspectorState.hoveredElement = target;
-        } else {
-          inspectorState.hoveredElement = undefined;
-        }
-        updateOverlay(inspectorState.hoveredElement, overlay, isActive);
-      },
-      { capture: true }
-    );
-
-    document.addEventListener(
-      "click",
-      (event) => {
-        if (isActive()) {
-          inspectorState.pressedKeys.clear();
-          const target = findContainer(event.target as HTMLElement);
-          const inspectUrl = target?.getAttribute("import");
-          if (inspectUrl) {
-            event.preventDefault();
-            document.body.style.setProperty("cursor", "progress");
-            openInEditor(inspectUrl, srcDir);
-          }
-        }
-      },
-      { capture: true }
-    );
-
-    document.addEventListener(
-      "contextmenu",
-      (event) => {
-        if (isActive()) {
-          inspectorState.pressedKeys.clear();
-          const target = findContainer(event.target as HTMLElement);
-          if (target) {
-            event.preventDefault();
-          }
-        }
-      },
-      { capture: true }
-    );
-
-    function checkKeysArePressed() {
-      const activeKeys = Array.from(inspectorState.pressedKeys).map((key) =>
-        key ? key.replace(/(Left|Right)$/g, "") : undefined
-      );
-      return hotKeys.every((key) => activeKeys.includes(key));
-    }
-
-    function isActive() {
-      return checkKeysArePressed();
-    }
-
-    window.addEventListener("resize", () =>
-      updateOverlay(inspectorState.hoveredElement, overlay, isActive)
-    );
-
-    document.addEventListener("scroll", () =>
-      updateOverlay(inspectorState.hoveredElement, overlay, isActive)
-    );
+    this.#showInfo();
 
     this.appendChild(
       Object.assign(document.createElement("style"), {
         textContent: this.styles,
       })
+    );
+
+    this.appendChild(
+      Object.assign(document.createElement("web-widget-inspector-info"), {
+        textContent: "Click-to-Source: " + hotKeys.join(" + "),
+      })
+    );
+
+    document.addEventListener("keydown", this.#addKeysHandleEvent.bind(this), {
+      capture: true,
+    });
+
+    document.addEventListener("keyup", this.#deleteKeysHandleEvent.bind(this), {
+      capture: true,
+    });
+
+    window.addEventListener("blur", this.#clearKeysHandleEvent.bind(this), {
+      capture: true,
+    });
+
+    document.addEventListener(
+      "mouseover",
+      this.#hoveredHandleEvent.bind(this),
+      { capture: true }
+    );
+
+    document.addEventListener("click", this.#clickHandEvent.bind(this), {
+      capture: true,
+    });
+
+    document.addEventListener(
+      "contextmenu",
+      this.#clearKeysHandleEvent.bind(this),
+      { capture: true }
+    );
+
+    window.addEventListener("resize", this.#updateOverlayHandEvent.bind(this));
+
+    document.addEventListener(
+      "scroll",
+      this.#updateOverlayHandEvent.bind(this)
     );
   }
 
@@ -228,17 +194,22 @@ export class HTMLWebWidgetInspectorElement extends HTMLElement {
 
   web-widget-inspector-overlay {
     position: fixed;
-    background: rgba(24, 182, 246, 0.27);
+    background: repeating-linear-gradient(
+      45deg,
+      rgba(68, 206, 246, 0.5),
+      rgba(68, 206, 246, 0.5) 5px,
+      rgba(255, 255, 255, 0.5) 5px,
+      rgba(255, 255, 255, 0.5) 10px
+    );
     pointer-events: none;
     box-sizing: border-box;
-    border: 1px solid rgba(24, 182, 246, 0.46);
-    border-radius: 4px;
+    border: 1px solid rgba(68, 206, 246, 0.9);
     contain: strict;
     cursor: pointer;
     z-index: 999999;
   }
 
-  web-widget-inspector-info-popup {
+  web-widget-inspector-info {
     position: fixed;
     bottom: 10px;
     right: 10px;
@@ -251,20 +222,9 @@ export class HTMLWebWidgetInspectorElement extends HTMLElement {
       0 20px 25px -5px rgb(0 0 0 / 34%),
       0 8px 10px -6px rgb(0 0 0 / 24%);
     backdrop-filter: blur(4px);
-    -webkit-animation: fadeOut 0.3s 3s ease-in-out forwards;
     animation: fadeOut 0.3s 3s ease-in-out forwards;
     z-index: 999999;
     contain: layout;
-  }
-
-  @-webkit-keyframes fadeOut {
-    0% {
-      opacity: 1;
-    }
-
-    100% {
-      opacity: 0;
-    }
   }
 
   @keyframes fadeOut {
