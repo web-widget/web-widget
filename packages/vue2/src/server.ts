9273bc86d0388982e5f46d428fe5ffb17e0a0ba8
糖饼 <sugarpie.tang@gmail.com>
2023-12-30 16:22:38 +0800

    refactor: Remove unfinished features

--- packages/vue2/src/server.ts
+++ packages/vue2/src/server.ts
@@ -2,13 +2,14 @@ import {
   defineRender,
   getComponentDescriptor,
 } from "@web-widget/schema/server-helpers";
-import { Readable } from "node:stream";
-import { TransformStream } from "node:stream/web";
+// import { Readable } from "node:stream";
+// import { TransformStream } from "node:stream/web";
 import Vue from "vue";
 import { createRenderer } from "vue-server-renderer";
 import type { CreateVueRenderOptions } from "./types";
 
 export * from "@web-widget/schema/server-helpers";
+export { useWidgetSyncState as useWidgetState } from "@web-widget/schema/server-helpers";
 export * from "./components";
 
 /**
@@ -23,7 +24,7 @@ Vue.config.warnHandler = (msg, vm, trace) => {
   console.error("[Vue warn]: ".concat(msg).concat(trace));
 };
 
-const __FEATURE_STREAM__ = false;
+// const __FEATURE_STREAM__ = false;
 
 const ESCAPE_LOOKUP: { [match: string]: string } = {
   ">": "\\u003e",
@@ -40,20 +41,20 @@ function htmlEscapeJsonString(str: string): string {
   return str.replace(ESCAPE_REGEX, (match) => ESCAPE_LOOKUP[match]);
 }
 
-function appendStringToReadableStream(
-  stream: ReadableStream,
-  text: string
-): ReadableStream {
-  const encoder = new TextEncoder();
-  const uint8Array = encoder.encode(text);
-  return stream.pipeThrough(
-    new TransformStream({
-      flush(controller) {
-        controller.enqueue(uint8Array);
-      },
-    })
-  );
-}
+// function appendStringToReadableStream(
+//   stream: ReadableStream,
+//   text: string
+// ): ReadableStream {
+//   const encoder = new TextEncoder();
+//   const uint8Array = encoder.encode(text);
+//   return stream.pipeThrough(
+//     new TransformStream({
+//       flush(controller) {
+//         controller.enqueue(uint8Array);
+//       },
+//     })
+//   );
+// }
 
 export const createVueRender = ({
   onBeforeCreateApp = async () => ({}),
@@ -94,19 +95,21 @@ export const createVueRender = ({
 
     await onCreatedApp(app, context, component, mergedProps);
 
-    const result =
-      __FEATURE_STREAM__ && Readable.toWeb
-        ? Readable.toWeb(renderer.renderToStream(app))
-        : await renderer.renderToString(app);
+    // const result =
+    //   __FEATURE_STREAM__ && Readable.toWeb
+    //     ? Readable.toWeb(renderer.renderToStream(app))
+    //     : await renderer.renderToString(app);
+    const result = await renderer.renderToString(app);
 
     app.$destroy();
 
     if (state) {
       const json = htmlEscapeJsonString(JSON.stringify(state));
       const script = `<script as="state" type="application/json">${json}</script>`;
-      return typeof result === "string"
-        ? result + script
-        : appendStringToReadableStream(result, script);
+      // return typeof result === "string"
+      //   ? result + script
+      //   : appendStringToReadableStream(result, script);
+      return result + script;
     } else {
       return result;
     }
