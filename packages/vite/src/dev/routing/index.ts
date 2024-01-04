483b3b370d38b27d9adf7b8cebfa4c64fcc563b7
糖饼 <sugarpie.tang@gmail.com>
2024-01-04 17:03:37 +0800

    refactor: fileSystemRouteGenerator

--- packages/vite/src/dev/routing/index.ts
+++ packages/vite/src/dev/routing/index.ts
@@ -5,15 +5,98 @@ import { pathToPattern, sortRoutePaths } from "./extract";
 import type { ManifestJSON } from "@web-widget/web-router";
 import { /*createFileId,*/ getPathnameFromDirPath, normalizePath } from "./fs";
 import type { RouteSourceFile } from "./types";
+import type { FSWatcher } from "vite";
 
-export async function rewriteRoutemap(
-  file: string,
-  routesDir: string,
+export type FileSystemRouteGeneratorOptions = {
+  basePathname: string;
+  root: string;
+  routemapPath: string;
+  routesPath: string;
+  trailingSlash: boolean;
+  update: (padding: Promise<void>) => void;
+  watcher: FSWatcher;
+};
+
+export async function fileSystemRouteGenerator({
+  basePathname,
+  root,
+  routemapPath,
+  routesPath,
+  trailingSlash,
+  update,
+  watcher,
+}: FileSystemRouteGeneratorOptions) {
+  const cache = {};
+  await generateRoutemapFile(
+    root,
+    routesPath,
+    basePathname,
+    trailingSlash,
+    routemapPath,
+    cache
+  );
+  const updateFileSystemRouteing = debounce(() => {
+    update(
+      generateRoutemapFile(
+        root,
+        routesPath,
+        basePathname,
+        trailingSlash,
+        routemapPath,
+        cache
+      )
+    );
+  }, 40);
+  watcher.on("all", (event, path) => {
+    if (path.startsWith(routesPath) && event !== "change") {
+      updateFileSystemRouteing();
+    }
+  });
+}
+
+function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number
+): (...args: Params) => void {
+  let timer: NodeJS.Timeout;
+  return (...args: Params) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
+
+async function generateRoutemapFile(
   root: string,
+  routesPath: string,
+  basePathname: string,
+  trailingSlash: boolean,
+  routemapPath: string,
+  cache: any
+) {
+  const key = Symbol.for("routemap");
+  const routemap = await getRoutemap(
+    root,
+    routesPath,
+    basePathname,
+    trailingSlash
+  );
+  const newJson = JSON.stringify(routemap, null, 2);
+
+  if (newJson !== cache[key]) {
+    await fs.writeFile(routemapPath, JSON.stringify(routemap, null, 2), "utf8");
+    cache[key] = newJson;
+  }
+}
+
+export async function getRoutemap(
+  root: string,
+  routesPath: string,
   basePathname: string,
   trailingSlash: boolean
-) {
-  const sourceFiles = await walkRoutes(routesDir);
+): Promise<ManifestJSON> {
+  const sourceFiles = await walkRoutes(routesPath);
   const fallbacks = sourceFiles.filter((s) => s.type === "fallback");
   const layouts = sourceFiles.filter((s) => s.type === "layout");
   const middlewares = sourceFiles
@@ -49,7 +132,7 @@ export async function rewriteRoutemap(
     };
   };
 
-  const routemap: ManifestJSON = {
+  return {
     routes: routes.map(toValue) as ManifestJSON["routes"],
     middlewares: middlewares.map(toValue) as ManifestJSON["middlewares"],
     fallbacks: fallbacks.map(toValue) as ManifestJSON["fallbacks"],
@@ -57,6 +140,4 @@ export async function rewriteRoutemap(
       ? toValue(layouts[0])
       : undefined) as ManifestJSON["layout"],
   };
-
-  await fs.writeFile(file, JSON.stringify(routemap, null, 2), "utf8");
 }
