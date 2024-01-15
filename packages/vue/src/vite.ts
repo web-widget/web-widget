dc3e7ce65cb7ba380f18422a64f4473d9f0e7c36
糖饼 <sugarpie.tang@gmail.com>
2024-01-15 11:50:35 +0800

    fix: Less and other languages cause errors

--- packages/vue/src/vite.ts
+++ packages/vue/src/vite.ts
@@ -2,7 +2,10 @@ import type { Plugin } from "vite";
 import { webWidgetPlugin } from "@web-widget/vite";
 import type { WebWidgetPluginOptions } from "@web-widget/vite";
 
-const EXCLUDE = /vue\?.*&lang\.(?:css|js|ts)$/;
+// Examples:
+// .vue?vue&type=script&setup=true&lang.ts
+// .vue?vue&type=style&index=0&scoped=7b8d5933&lang.less
+const VUE_INTERNAL_REQUEST = /vue\?vue\b.*$/;
 
 function toArray(value: any) {
   return Array.isArray(value) ? value : value ? [value] : [];
@@ -20,13 +23,13 @@ export default function vueWebWidgetPlugin({
     export: {
       include: /(?:\.|@)(?:route|widget)\.vue(?:\?.*)?$/,
       ...exportWidget,
-      exclude: [...toArray(exportWidget.exclude), EXCLUDE],
+      exclude: [...toArray(exportWidget.exclude), VUE_INTERNAL_REQUEST],
     },
     import: {
       include: /(?:\.|@)widget\..*$/,
       includeImporter: /.*\.vue(?:\?.*)?$/,
       ...importWidget,
-      exclude: [...toArray(importWidget.exclude), EXCLUDE],
+      exclude: [...toArray(importWidget.exclude), VUE_INTERNAL_REQUEST],
     },
   });
 }
