7f87deb17c6368505d4343bcf33ca46d7577e1f3
糖饼 <sugarpie.tang@gmail.com>
2024-02-10 21:18:11 +0800

    test: replace monorepo root with a placeholder to make the snapshot stable

--- examples/web-router/test/server.ts
+++ examples/web-router/test/server.ts
@@ -1,4 +1,7 @@
+import { fileURLToPath } from 'node:url';
 import { createServer } from 'vite';
+
+const MONOREPO_ROOT = fileURLToPath(new URL('../../../', import.meta.url));
 
 export type Server = {
   close: () => Promise<void>;
@@ -19,8 +22,20 @@ export async function createTestServer(): Promise<Server> {
   viteDevServer.bindCLIShortcuts({ print: true });
 
   const close = () => viteDevServer.close();
-  const request = (pathname: string, options?: RequestInit) =>
-    fetch(`http://localhost:${port}${pathname}`, options);
+  const request = (pathname: string, options?: RequestInit) => {
+    const res = fetch(`http://localhost:${port}${pathname}`, options).then(
+      (res) => {
+        const text = res.text;
+        res.text = async () => {
+          const t = await text.call(res);
+          // NOTE: Replace monorepo root with a placeholder to make the snapshot stable.
+          return t.replaceAll(MONOREPO_ROOT, '#TEST_MONOREPO_ROOT#');
+        };
+        return res;
+      }
+    );
+    return res;
+  };
 
   return { close, request };
 }
