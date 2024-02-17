66075ebd1ba445383f7760972a4157ffe4d25f78
糖饼 <sugarpie.tang@gmail.com>
2024-02-17 18:32:19 +0800

    test: update snapshots

--- examples/basic/test/snapshots.test.ts
+++ examples/basic/test/snapshots.test.ts
@@ -1,6 +1,10 @@
+import routes from '../routemap.server.json';
 import { createTestServer, type Server } from './server';
 
 let server: Server;
+const STATIC_ROUTES = routes.routes.filter(
+  ({ pathname }) => !/[:(){}*+?]/.test(pathname)
+);
 
 beforeAll(async () => {
   server = await createTestServer();
@@ -11,11 +15,11 @@ afterAll(async () => {
 });
 
 describe('Should match snapshot', () => {
-  test.each([['/about'], ['/']])(
+  test.each(STATIC_ROUTES.map((route) => [route.pathname]))(
     'Request "%s" should match snapshot',
-    async (pathname, status = 200) => {
+    async (pathname) => {
       const result = await server.fetch(`${pathname}`);
-      expect(result.status).toBe(status);
+      expect(result.status).toMatchSnapshot(`${pathname}@status`);
       expect(result.statusText).toMatchSnapshot(`${pathname}@statusText`);
       expect(result.headers).toMatchSnapshot(`${pathname}@headers`);
       expect(await result.text()).toMatchSnapshot(`${pathname}@body`);
