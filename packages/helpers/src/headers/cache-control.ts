9ad8336cdbbb005c1307e8154eef6f7616a72ba5
糖饼 <sugarpie.tang@gmail.com>
2024-03-07 16:46:36 +0800

    refactor

--- packages/helpers/src/headers/cache-control.ts
+++ packages/helpers/src/headers/cache-control.ts
@@ -1,20 +1,18 @@
 export function cacheControl(headers: Headers, cacheControl: string) {
   const directives = cacheControl
-    ?.split(',')
+    .split(',')
     .map((directive) => directive.toLowerCase());
 
-  if (directives) {
-    const existingDirectives =
-      headers
-        .get('Cache-Control')
-        ?.split(',')
-        .map((d) => d.trim().split('=', 1)[0]) ?? [];
-    for (const directive of directives) {
-      let [name, value] = directive.trim().split('=', 2);
-      name = name.toLowerCase();
-      if (!existingDirectives.includes(name)) {
-        headers.append('Cache-Control', `${name}${value ? `=${value}` : ''}`);
-      }
+  const existingDirectives =
+    headers
+      .get('Cache-Control')
+      ?.split(',')
+      .map((d) => d.trim().split('=', 1)[0]) ?? [];
+  for (const directive of directives) {
+    let [name, value] = directive.trim().split('=', 2);
+    name = name.toLowerCase();
+    if (!existingDirectives.includes(name)) {
+      headers.append('Cache-Control', `${name}${value ? `=${value}` : ''}`);
     }
   }
 }
