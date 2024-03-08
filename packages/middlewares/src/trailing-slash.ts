5a144c04cce030de460f92637e320945b08c0306
糖饼 <sugarpie.tang@gmail.com>
2024-03-08 12:03:01 +0800

    refactor

--- packages/middlewares/src/trailing-slash.ts
+++ packages/middlewares/src/trailing-slash.ts
@@ -1,46 +1,30 @@
-import type { MiddlewareContext } from '@web-widget/helpers';
 import { defineMiddlewareHandler } from '@web-widget/helpers';
 import { Status } from '@web-widget/helpers/status';
 import { redirect } from '@web-widget/helpers/navigation';
 
-declare module '@web-widget/schema' {
-  interface RouteConfig {
-    trailingSlash?: boolean;
-  }
-}
-
 export type TrailingSlashOptions = {
   trailingSlash?: boolean;
-  filter?: (ctx: MiddlewareContext) => boolean;
 };
 
 export const trailingSlash = (options: TrailingSlashOptions = {}) => {
+  const trailingSlash = options.trailingSlash ?? false;
   return defineMiddlewareHandler(
     async function trailingSlashMiddleware(context, next) {
       const url = new URL(context.request.url);
-      const trailingSlash =
-        options.trailingSlash ?? context.module?.config?.trailingSlash;
+      const { pathname, search } = url;
 
       // Redirect requests that end with a trailing slash to their non-trailing
       // slash counterpart.
       // Ex: /about/ -> /about
-      if (
-        url.pathname.length > 1 &&
-        url.pathname.endsWith('/') &&
-        !trailingSlash
-      ) {
+      if (pathname.length > 1 && pathname.endsWith('/') && !trailingSlash) {
         // Remove trailing slashes
-        const path = url.pathname.replace(/\/+$/, '');
-        const location = `${path}${url.search}`;
+        const path = pathname.replace(/\/+$/, '');
+        const location = `${path}${search}`;
         return redirect(location, Status.PermanentRedirect);
-      } else if (trailingSlash && !url.pathname.endsWith('/')) {
-        const filter =
-          options.filter ??
-          // If the last element of the path has a "." it's a file
-          (() => url.pathname.split('/').at(-1)?.includes('.'));
-        const ignore = filter(context) ?? false;
-
-        if (!ignore) {
+      } else if (trailingSlash && !pathname.endsWith('/')) {
+        // If the last element of the path has a "." it's a file
+        const isFile = pathname.split('/').at(-1)?.includes('.');
+        if (!isFile) {
           url.pathname += '/';
           return redirect(url.href, Status.PermanentRedirect);
         }
