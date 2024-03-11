74e573872478b1800dcf2873d009cdeb97253bff
糖饼 <sugarpie.tang@gmail.com>
2024-03-11 23:53:41 +0800

    feat: retainedHeaders (#372)

--- packages/middlewares/src/conditional-get.ts
+++ packages/middlewares/src/conditional-get.ts
@@ -3,11 +3,36 @@ import { defineMiddlewareHandler } from '@web-widget/helpers';
 import { Status, STATUS_TEXT } from '@web-widget/helpers/status';
 import { isFresh } from './utils/is-fresh';
 
-export default function conditionalGet() {
+/**
+ * Default headers to pass through on 304 responses. From the spec:
+ * > The response must not contain a body and must include the headers that
+ * > would have been sent in an equivalent 200 OK response: Cache-Control,
+ * > Content-Location, Date, ETag, Expires, and Vary.
+ */
+const RETAINED_304_HEADERS = [
+  'cache-control',
+  'content-location',
+  'date',
+  'etag',
+  'expires',
+  'vary',
+];
+
+export type ConditionalGetOptions = {
+  retainedHeaders?: string[];
+};
+
+export default function conditionalGet(options: ConditionalGetOptions = {}) {
+  const retainedHeaders = options?.retainedHeaders ?? RETAINED_304_HEADERS;
   return defineMiddlewareHandler(
     async function conditionalGetMiddleware(ctx, next) {
       const res = await next();
       if (isFresh(ctx.request, res)) {
+        res.headers.forEach((_, key) => {
+          if (!retainedHeaders.includes(key.toLowerCase())) {
+            res.headers.delete(key);
+          }
+        });
         return new Response(null, {
           status: Status.NotModified,
           statusText: STATUS_TEXT[Status.NotModified],
