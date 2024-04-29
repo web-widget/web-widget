bb1f12991d045c80134ebef02c5f9e7fd110c788
糖饼 <sugarpie.tang@gmail.com>
2024-04-29 19:13:39 +0800

    fix: types error

--- packages/helpers/src/state/widget-state.ts
+++ packages/helpers/src/state/widget-state.ts
@@ -5,24 +5,10 @@ import {
 import { useRouteState } from './route-state';
 
 /** @deprecated Use `import { cacheAsyncProvider } from '@web-widget/helpers/cache'` instead. */
-export const useWidgetAsyncState = (
-  ...args: Parameters<typeof cacheAsyncProvider>
-) => {
-  console.warn(
-    "Use `import { cacheAsyncProvider } from '@web-widget/helpers/cache'` instead."
-  );
-  return cacheAsyncProvider(args[0], args[1], true);
-};
+export const useWidgetAsyncState = cacheAsyncProvider;
 
 /** @deprecated Use `import { cacheSyncProvider } from '@web-widget/helpers/cache'` instead. */
-export const useWidgetSyncState = (
-  ...args: Parameters<typeof cacheSyncProvider>
-) => {
-  console.warn(
-    "Use `import { cacheSyncProvider } from '@web-widget/helpers/cache'` instead."
-  );
-  return cacheSyncProvider(args[0], args[1], true);
-};
+export const useWidgetSyncState = cacheSyncProvider;
 
 /**
  * @deprecated
