72b917983d557cd9eb0fc6eb8e37c0afede01fed
糖饼 <sugarpie.tang@gmail.com>
2024-08-20 12:31:33 +0800

    feat: `importShim` is optional (#510)

---
+++ .changeset/rude-eggs-help.md
@@ -0,0 +1,6 @@
+---
+'@web-widget/vite-plugin': minor
+'@web-widget/web-router': minor
+---
+
+`importShim` is optional.
--- playgrounds/router/routes/_root@layout.ts
+++ playgrounds/router/routes/_root@layout.ts
@@ -6,44 +6,6 @@ import { renderMetaToString } from '@web-widget/helpers';
 
 export { render };
 
-const importShimLoader = html`<script id="shim:es-module">
-  if (!HTMLScriptElement.supports || !HTMLScriptElement.supports('importmap')) {
-    self.importShim = Object.assign(
-      function importShimProxy() {
-        const esModuleShimUrl =
-          'https://ga.jspm.io/npm:es-module-shims@1.7.3/dist/es-module-shims.js';
-        const promise = new Promise((resolve, reject) => {
-          document.head.appendChild(
-            Object.assign(document.createElement('script'), {
-              src: esModuleShimUrl,
-              crossorigin: 'anonymous',
-              async: true,
-              onload() {
-                if (!importShim.$proxy) {
-                  resolve(importShim);
-                } else {
-                  reject(
-                    new Error(
-                      'No globalThis.importShim found:' + esModuleShimUrl
-                    )
-                  );
-                }
-              },
-              onerror(error) {
-                reject(error);
-              },
-            })
-          );
-        });
-        return promise.then((importShim) => importShim(...arguments));
-      },
-      {
-        $proxy: true,
-      }
-    );
-  }
-</script>`;
-
 export default function RootLayout({
   meta,
   children,
@@ -52,7 +14,7 @@ export default function RootLayout({
   return html`<!doctype html>
     <html lang="${meta.lang}">
       <head>
-        ${importShimLoader} ${unsafeHTML(renderMetaToString(meta))}
+        ${unsafeHTML(renderMetaToString(meta))}
       </head>
       <body>
         ${children instanceof ReadableStream
