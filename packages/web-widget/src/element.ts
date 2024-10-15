e31a8d63cc3664044e2cc263ba57d1acf48a0942
糖饼 <sugarpie.tang@gmail.com>
2024-10-15 16:47:39 +0800

    feat(web-widget): record performance (#567)

--- packages/web-widget/src/element.ts
+++ packages/web-widget/src/element.ts
@@ -24,6 +24,11 @@ const innerHTMLDescriptor = Object.getOwnPropertyDescriptor(
 )!;
 const innerHTMLSetter = innerHTMLDescriptor.set!;
 
+export type PerformanceMarkDetail = {
+  name: string;
+  import: string;
+};
+
 export const INNER_HTML_PLACEHOLDER = `<!--web-widget:placeholder-->`;
 
 /**
@@ -569,12 +574,45 @@ export class HTMLWebWidgetElement extends HTMLElement {
       this.removeAttribute('recovering');
     }
 
+    const name = this.localName;
+    const markNameSpace = `${name}:statusChange`;
+    const detail: PerformanceMarkDetail = {
+      name: this.#name,
+      import: this.import,
+    };
+
+    performance.mark(`${markNameSpace}:${value}`, {
+      detail,
+    });
+
+    switch (value) {
+      case status.LOADED:
+        performance.measure(`${name}:load`, {
+          start: `${markNameSpace}:${status.LOADING}`,
+          end: `${markNameSpace}:${status.LOADED}`,
+          detail,
+        });
+        break;
+      case status.MOUNTED:
+        performance.measure(`${name}:mount`, {
+          start: `${markNameSpace}:${status.MOUNTING}`,
+          end: `${markNameSpace}:${status.MOUNTED}`,
+          detail,
+        });
+        break;
+    }
+
     this.dispatchEvent(new Event('statuschange'));
   }
 
+  get #name() {
+    return (
+      this.id || this.getAttribute('name') || this.import || this.localName
+    );
+  }
+
   #throwGlobalError(error: Error) {
-    const moduleName =
-      this.id || this.getAttribute('name') || this.import || this.localName;
+    const moduleName = this.#name;
     const prefix = `Web Widget module (${moduleName})`;
     if (typeof error !== 'object') {
       error = new Error(error, {
