c10a111e0e10bcf7115f6c08f4a0c868a2313771
糖饼 <sugarpie.tang@gmail.com>
2024-10-26 15:48:33 +0800

    test: clear test code

--- packages/vue/tsup.config.ts
+++ packages/vue/tsup.config.ts
@@ -1,27 +1,5 @@
 import type { Options } from 'tsup';
 
-import fs from 'node:fs';
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const existsSync = (filePath: string) => {
-  if (fs.existsSync(filePath)) {
-    console.log('>>>>File exists:', filePath);
-  } else {
-    console.log('>>>>File does not exist', filePath);
-  }
-};
-
-existsSync(`${__dirname}/package.json`);
-existsSync(`${__dirname}/node_modules/vite/package.json`);
-existsSync(`${__dirname}/node_modules/@web-widget/helpers/package.json`);
-existsSync(`${__dirname}/node_modules/@web-widget/react/package.json`);
-existsSync(`${__dirname}/node_modules/@web-widget/react/dist/react.server.js`);
-existsSync(
-  `${__dirname}/node_modules/@web-widget/react/dist/react.server.d.ts`
-);
-
 const baseOptions: Options = {
   dts: true,
   target: 'es2017',
