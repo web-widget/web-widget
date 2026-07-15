154182892e606a53c7cf594533189aeb60cb4963
糖饼 <sugarpie.tang@gmail.com>
2026-07-15 23:58:13 +0800

    fix(web-router): preserve schema type exports

--- packages/web-router/src/types.ts
+++ packages/web-router/src/types.ts
@@ -10,15 +10,28 @@ import type {
   RouteModule,
   ServerRender,
   ServerRenderResult,
-} from '@web-widget/helpers';
+} from '@web-widget/schema';
 
 import type { Context } from './context';
 export type {
+  ActionModule,
+  FetchContext,
+  FetchEventLike,
+  HTTPException,
   Meta,
+  MiddlewareContext,
+  MiddlewareHandler,
+  MiddlewareModule,
+  RouteComponentProps,
   RouteContext,
+  RouteFallbackComponentProps,
+  RouteHandler,
+  RouteHandlers,
   RouteModule,
   RouteRenderOptions,
+  ServerRender,
   ServerRenderOptions,
+  ServerRenderResult,
 } from '@web-widget/schema';
 
 ////////////////////////////////////////
